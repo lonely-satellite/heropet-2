a761fd885ac13a0dd6e6ee95f610114157511f72
Luke Kaalim <lukekaalim@gmail.com>
2022-12-10 15:17:34 +1100

    Implement catbard

--- web/app/App.js
+++ web/app/App.js
@@ -3,8 +3,11 @@
 import type { Component } from '@lukekaalim/act';
 */
 
+import { h } from "@lukekaalim/act"
+
 export const App/*: Component<>*/ = () => {
   return [
-    'Welcome to hero pet!'
+    'Welcome to hero pet!',
+    h('img', { src: './assets/catbard.png' }),
   ]
 }
